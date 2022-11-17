886f1f310a83e16e5f6ed2c0e00d9ca9f01e4c60
Szymon Cader <szymon.cader@deployed.pl>
2022-11-17 14:24:18 +0000

    Adjust local settings

--- santa_claus/settings/local.py
+++ santa_claus/settings/local.py
@@ -6,9 +6,11 @@ SECRET_KEY = "secret_key"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DB", "santa_claus"),
-        "USER": env("POSTGRES_USER", "santa_claus"),
-        "PASSWORD": env("POSTGRES_PASSWORD", "santa_claus"),
+        "NAME": env("POSTGRES_DB", "postgres"),
+        "USER": env("POSTGRES_USER", "postgres"),
+        "PASSWORD": env("POSTGRES_PASSWORD", "postgres"),
         "HOST": env("POSTGRES_HOST", "localhost"),
     }
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://nekeal-expert-palm-tree-p76xvwwvg7x267g6-8000.preview.app.github.dev"]
